9c11d000ba9b5de50bbf3e7afa2ce57868bf7b92
serotonine <ks.serotonine@gmail.com>
2024-09-24 13:34:50 +0200

    Master: Implements HttpClient.

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +13,27 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
+  constructor(
+    private _httpClient: HttpClient,
+    private _destroyRef: DestroyRef
+  ) {}
   places = signal<Place[] | undefined>(undefined);
+
+  ngOnInit() {
+    const subscription = this._httpClient
+      /* {observe:'response"} is not mandatory.
+     Could add add another param in subscribe. 
+     */
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(map((datas) => datas.places))
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+      });
+    this._destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
 
+bootstrapApplication(AppComponent, { providers: [provideHttpClient()] }).catch(
+  (err) => console.error(err)
+);
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+/*
+ Providing HttpClient when using NgModules : 
+ https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/44127430#overview
+ */
