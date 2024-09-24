6e03756473e88ddca119e8e1ebfe128d725b82fb
serotonine <ks.serotonine@gmail.com>
2024-09-24 14:02:36 +0200

    Master: Handling http request error.

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,8 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -19,17 +20,33 @@ export class AvailablePlacesComponent implements OnInit {
     private _destroyRef: DestroyRef
   ) {}
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
 
   ngOnInit() {
-    const subscription = this._httpClient
-      /* {observe:'response"} is not mandatory.
+    this.isFetching.set(true);
+
+    /* {observe:'response"} is not mandatory.
      Could add add another param in subscribe. 
      */
+    const subscription = this._httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
-      .pipe(map((datas) => datas.places))
+      .pipe(
+        map((datas) => datas.places),
+        catchError((error) =>
+          throwError(() => new Error('Problem with the request'))
+        )
+      )
       .subscribe({
         next: (places) => {
           this.places.set(places);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+        error: (error: Error) => {
+          console.log(error.message);
+          this.error.set('Problem with the request');
         },
       });
     this._destroyRef.onDestroy(() => {
